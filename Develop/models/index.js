035c71e0d68bfba976b292eb4304ac44dd8aa0dd
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-18 15:57:21 -0500

    Updated models/index.js Product, Catgeory

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,26 +4,28 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Products belongsTo Category//
 Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
-
-// Categories have many Products
+// Categories have many Products//
 Category.hasMany(Product, {
   foreignKey: "category_id",
 });
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag)//
 Product.belongsToMany(Tag, {
+  through: ProductTag,
+
   foreignKey: "tag_id",
 });
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag)//
 Tag.belongsToMany(Product, {
+  through: ProductTag,
+
   foreignKey: "product_id",
 });
-
 module.exports = {
   Product,
   Category,

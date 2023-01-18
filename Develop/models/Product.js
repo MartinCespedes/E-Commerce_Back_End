581e3fcd583d7e7742c081b57a125686fe754554
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-18 14:44:27 -0500

    Created Product catergories in Product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,23 +1,46 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const Category = require("./Category");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
+Product.init({
+  id: {
+    type: DataTypes.INTERGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncriment: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  stock: {
+    type: DataTypes.INTERGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
+  },
+  category_id: {
+    type: DataTypes.INTERGER,
+    references: {
+      model: "category",
+      key: "id",
+    },
+  },
+});
 
 module.exports = Product;

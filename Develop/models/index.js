438fca59440424026c908711f6b8055f0eeefcd6
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-18 15:00:41 -0500

    Created associations between product, catgeory, product, tag & product tag

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,28 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "tag_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "product_id",
+});
 
 module.exports = {
   Product,

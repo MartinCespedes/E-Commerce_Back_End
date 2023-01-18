53fbeabe06aaf7e1c342d2a5d76494db1892eb60
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-18 16:26:21 -0500

    Added more attributes to routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -11,7 +11,7 @@ router.get("/", (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ["id", "product_name", "price"],
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
       },
     ],
   })
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,11 +7,11 @@ router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   Tag.findAll({
-    attributes: ["id"],
+    attributes: ["id", "tag_name"],
     include: [
       {
         model: Product,
-        attributes: ["product_name", "price", "stock"],
+        attributes: ["id", "product_name", "price", "stock"],
       },
     ],
   })
